f87d5482047136450bbc411f628db2ffd0c420fe
Hecate2 <2474101468@qq.com>
2019-12-31 23:39:39 +0800

    Update 安装依赖库.bat

--- 安装依赖库.bat
+++ 安装依赖库.bat
@@ -1,4 +1,4 @@
-start pip install gevent
-start pip install requests
-start pip install tornado
-pip install pillow
+start pip install -i https://pypi.tuna.tsinghua.edu.cn/simple gevent
+start pip install -i https://pypi.tuna.tsinghua.edu.cn/simple requests
+start pip install -i https://pypi.tuna.tsinghua.edu.cn/simple tornado
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pillow
