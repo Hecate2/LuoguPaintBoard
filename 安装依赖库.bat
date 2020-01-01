5adfab2bf6bcaf97b89964904b4ab1a9c6cbeae3
Hecate2 <2474101468@qq.com>
2020-01-01 12:07:50 +0800

    Update 安装依赖库.bat

--- 安装依赖库.bat
+++ 安装依赖库.bat
@@ -1,4 +1,4 @@
 start python3 -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple gevent
 start python3 -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple requests
 start python3 -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple tornado
-pip python3 -m install -i https://pypi.tuna.tsinghua.edu.cn/simple pillow
+python3 -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pillow
